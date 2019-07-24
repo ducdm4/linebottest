6a21100d3babd65589b2e12465ba985ca0a10676
duc.duong <duc.duong@ntq-solution.com.vn>
2019-07-24 14:30:49 +0700

    no message

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
-file_put_contents ( 'test.log' , "11\r\n" );
+file_put_contents ( 'test.log' , "11: ".$request_array."\r\n" );
 
 if ( sizeof($request_array['events']) > 0 ) {
 
